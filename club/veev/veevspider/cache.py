8111ef0a4961c7b95a785ded2fe271a9d47f97fc
Veev <384506557@qq.com>
2017-09-09 23:23:02 +0800

    设计 线程数量

--- club/veev/veevspider/cache.py
+++ club/veev/veevspider/cache.py
@@ -1,43 +1,50 @@
 import pymysql
 
 
-def get(site='default'):
-    d = ProxyStorage()
-    proxy = d.get(site)
-    d.close()
-    if not proxy:
-        return None
-    p = {"http": "http://{}:{}".format(proxy[3], proxy[4]),
-         "https": "https://{}:{}".format(proxy[3], proxy[4])}
-    return p
-
-
-class ProxyStorage:
-    def __init__(self, site='default'):
-        self.__site = site
+class Cache:
+    def __init__(self, room='cache'):
+        self.__room = room
         self.__reset()
         pass
 
-    def put(self, ip, port, site):
-        self.__insert_item(self.__site if site else site, ip, port)
+    def set(self, k, v):
+        result = self.__query_item(k)
+        if len(result) != 0:
+            id = result[0][0]
+            self.__update_item(id, v)
+        else:
+            self.__insert_item(k, v)
         pass
 
-    def get(self, site):
+    def get(self, k):
         # SQL 查询语句
-        result = self.__query_item(self.__site if site else site)
+        result = self.__query_item(k)
         if result:
-            self.__delete_item(result[0][0])
-            return result[0]
+            return result[0][3]
         return None
+
+    def remove(self, k):
+        result = self.__query_item(k)
+        if result:
+            id = result[0][0]
+            self.__delete_item(id)
+        pass
+
+    def clear(self, k):
+        pass
+
+    def has_key(self, k):
+        result = self.__query_item(k)
+        return True if len(result) != 0 else False
 
     def __reset(self):
         self.__db = pymysql.connect("127.0.0.1", "root", "123456", "py_test")
         self.__cursor = self.__db.cursor()
         pass
 
-    def __query_item(self, site):
+    def __query_item(self, k):
         # SQL 查询语句
-        sql = "SELECT * FROM proxy WHERE site = '%s'" % (site)
+        sql = "SELECT * FROM kv WHERE room = '%s' and k = '%s'" % (self.__room, k)
         results = []
         try:
             # self.__reset()
@@ -51,12 +58,9 @@ class ProxyStorage:
             # self.__close()
             return results
 
-    def __insert_item(self, site, ip, port):
+    def __insert_item(self, k, v):
         # SQL 查询语句
-        proxy = ip + ':' + port
-        sql = "INSERT INTO proxy(site, proxy, ip, port) VALUES ('%s', '%s', '%s', '%s')" \
-              % (site, proxy, ip, port)
-        print(sql)
+        sql = "INSERT INTO kv(room, k, v) VALUES ('%s', '%s', '%s')" % (self.__room, k, v)
         try:
             # self.__reset()
             # 执行SQL语句
@@ -69,9 +73,23 @@ class ProxyStorage:
         # finally:
         #     self.__close()
 
+    def __update_item(self, id, v):
+        # SQL 查询语句
+        sql = "UPDATE kv SET v='%s' WHERE id='%s'" % (v, id)
+        try:
+            # self.__reset()
+            # 执行SQL语句
+            self.__cursor.execute(sql)
+            self.__db.commit()
+        except Exception as e:
+            print(e)
+            self.__db.rollback()
+        # finally:
+        #     self.__close()
+
     def __delete_item(self, id):
         # SQL 查询语句
-        sql = "DELETE FROM proxy WHERE id='%s'" % (id)
+        sql = "DELETE FROM kv WHERE id='%s'" % (id)
         try:
             # self.__reset()
             # 执行SQL语句
@@ -88,18 +106,11 @@ class ProxyStorage:
             self.__db.close()
         pass
 
-    def close(self):
-        self.__close()
-
-    pass
-
-
-class ProxySpider:
-    def __init__(self):
-        pass
     pass
 
 
 if __name__ == '__main__':
-    # c = Cache('https://nj.lianjia.com')
-    print(get('https://nj.lianjia.com'))
+    cache = Cache('d')
+    cache.set('k2', 'v2')
+    print(cache.get('k2'))
+
---
+++ club/veev/veevspider/proxy_helper.py
@@ -0,0 +1,221 @@
+# -*- coding: utf-8 -*-
+"""
+代理帮助模块
+"""
+
+import pymysql
+import requests
+import threading
+import header_helper as header
+from lxml import etree
+from bs4 import BeautifulSoup
+
+test_url = ['https://www.lianjia.com']
+
+
+def get(site='default'):
+    d = _ProxyStorage()
+    proxy = d.get(site)
+    d.close()
+    if not proxy:
+        return None
+    p = {"http": "http://{}:{}".format(proxy[3], proxy[4]),
+         "https": "https://{}:{}".format(proxy[3], proxy[4])}
+    return p
+
+
+class _ProxyStorage:
+    def __init__(self, site='default'):
+        self.__site = site
+        self.__reset()
+        pass
+
+    def put(self, ip, port, site):
+        self.__insert_item(site if site else self.__site, ip, port)
+        pass
+
+    def get(self, site):
+        # SQL 查询语句
+        result = self.__query_item(site if site else self.__site)
+        if result:
+            self.__delete_item(result[0][0])
+            return result[0]
+        return None
+
+    def __reset(self):
+        self.__db = pymysql.connect("127.0.0.1", "root", "123456", "py_test")
+        self.__cursor = self.__db.cursor()
+        pass
+
+    def __query_item(self, site):
+        # SQL 查询语句
+        sql = "SELECT * FROM proxy WHERE site = '%s'" % (site)
+        results = []
+        try:
+            # 执行SQL语句
+            self.__cursor.execute(sql)
+            # 获取所有记录列表
+            results = self.__cursor.fetchall()
+        except Exception as e:
+            print('查询数据异常', e)
+        finally:
+            return results
+
+    def __insert_item(self, site, ip, port):
+        # SQL 查询语句
+        proxy = ip + ':' + port
+        sql = "INSERT INTO proxy(site, proxy, ip, port) VALUES ('%s', '%s', '%s', '%s')" \
+              % (site, proxy, ip, port)
+        try:
+            # 执行SQL语句
+            self.__cursor.execute(sql)
+            # 提交到数据库执行
+            self.__db.commit()
+        except Exception as e:
+            print('插入数据异常', e)
+            self.__db.rollback()
+
+    def __delete_item(self, id):
+        # SQL 查询语句
+        sql = "DELETE FROM proxy WHERE id='%s'" % (id)
+        try:
+            # 执行SQL语句
+            self.__cursor.execute(sql)
+            self.__db.commit()
+        except Exception as e:
+            print('删除数据异常', e)
+            self.__db.rollback()
+
+    def __close(self):
+        if not self.__db._closed:
+            self.__db.close()
+        pass
+
+    def close(self):
+        self.__close()
+
+    pass
+
+
+class ProxySpider:
+    def __init__(self):
+        # 存储单元
+        global _ps
+        # 锁 - 存储
+        global _lock_storage
+        global _quene_proxy
+        _ps = _ProxyStorage()
+        _lock_storage = threading.Lock()
+        _quene_proxy = []
+        pass
+
+    def start(self):
+        # 爬
+        # self.get_xi_ci()
+        # self.get_data_5u()
+        self.get_ip_181()
+
+        # 校验
+        self.__check()
+        pass
+
+    def get_xi_ci(self):
+        """
+        西刺代理
+        """
+        url_list = ('http://www.xicidaili.com/nn',  # 高匿
+                    'http://www.xicidaili.com/nt',  # 透明
+                    )
+        __count = 0
+        for url in url_list:
+            r = requests.get(url=url, headers=header.get_header())
+            if r.status_code == 200:
+                soup = BeautifulSoup(r.text, "lxml")
+                odd = soup.find_all('tr', {'class': 'odd'})
+                for o in odd:
+                    ts = o.find_all('td')
+                    proxy = (ts[1].text, ts[2].text)
+                    _quene_proxy.append(proxy)
+                    __count += 1
+        print('------- 西刺爬取完成, 共计 %d 条 -------' % __count)
+        pass
+
+    def get_data_5u(self):
+        """
+        无忧代理
+        """
+        url_list = ('http://www.data5u.com/free/gngn/index.shtml',  # 高匿
+                    'http://www.data5u.com/free/gnpt/index.shtml',  # 普通
+                    )
+        __count = 0
+        for url in url_list:
+            r = requests.get(url=url, headers=header.get_header())
+            if r.status_code == 200:
+                tree = etree.HTML(r.text)
+                ul_list = tree.xpath('//ul[@class="l2"]')
+                for ul in ul_list:
+                    proxy = (ul.xpath('.//li/text()')[0], ul.xpath('.//li/text()')[1])
+                    _quene_proxy.append(proxy)
+                    __count += 1
+        print('------- 无忧爬取完成, 共计 %d 条 -------' % __count)
+
+    def get_ip_181(self):
+        """
+        ip181
+        """
+        url = 'http://www.ip181.com/'
+        __count = 0
+        r = requests.get(url=url, headers=header.get_header())
+        if r.status_code == 200:
+            tree = etree.HTML(r.text)
+            tr_list = tree.xpath('//tr')[1:]
+            for tr in tr_list:
+                proxy = (tr.xpath('./td/text()')[0], tr.xpath('./td/text()')[1])
+                _quene_proxy.append(proxy)
+                __count += 1
+        print('------- ip181爬取完成, 共计 %d 条 -------' % __count)
+
+    def __check(self):
+        while _quene_proxy:
+            while len(threading.enumerate()) < 64:
+                _Checker(_quene_proxy.pop(0)).start()
+        pass
+
+    pass
+
+
+class _Checker(threading.Thread):
+    """
+    代理校验类 
+    """
+
+    def __init__(self, proxy):
+        threading.Thread.__init__(self)
+        self.p = proxy
+
+    def run(self):
+        try:
+            p = {"http": "http://{}:{}".format(self.p[0], self.p[1]),
+                 "https": "https://{}:{}".format(self.p[0], self.p[1])}
+            for url in test_url:
+                r = requests.get(url=url,
+                                 headers=header.get_header(),
+                                 timeout=20,
+                                 proxies=p)
+                if r.status_code == 200:
+                    print('代理测试通过: ', self.p)
+                    # 获取锁:
+                    _lock_storage.acquire()
+                    try:
+                        _ps.put(self.p[0], self.p[1], url)
+                    finally:
+                        # 释放锁:
+                        _lock_storage.release()
+        except BaseException as e:
+            print('代理测试失败, 异常 ---> ', self.p, e)
+
+
+if __name__ == '__main__':
+    spider = ProxySpider()
+    spider.start()
+    pass
